4a752190b737c511049e8be59d11a1b0aa71d16d
Nazik <nazikbaktygulova77@gmail.com>
2023-03-23 18:01:43 +0600

    Написан код для задачи №47 в папке hw_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double FillArray(int m, int n) 
+{
+    double[,] array = new double[m, n];
+ 
+            Random random = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.NextDouble()*100;
+                    Console.Write("{0,6:F2}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+    return 0;
+}
+
+Console.WriteLine("Введите количество строк: "); 
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: "); 
+int N = Convert.ToInt32(Console.ReadLine());
+
+FillArray(M, N);
